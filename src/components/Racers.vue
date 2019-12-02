346386eeef823a752a9b7e8e968b86769933a959
Dmitriy89k <d.v.kukshyn@gmail.com>
2019-12-02 21:09:58 +0200

    add offset

--- src/components/Racers.vue
+++ src/components/Racers.vue
@@ -1,37 +1,39 @@
 <template>
     <div>
-      <el-table
-            :data="racersDisplay"
-            style="width: 100%">
-        <el-table-column
-                prop="familyName"
-                label="Family name"
-                width="180">
-        </el-table-column>
-        <el-table-column
-                prop="givenName"
-                label="First name"
-                width="180">
-        </el-table-column>
-        <el-table-column
-                prop="dateOfBirth"
-                label="Date of birth"
-                width="180">
-        </el-table-column>
-        <el-table-column
-                 label="biography">
-            <template slot-scope="scope">
-                <a v-bind:href= getUrl(scope.$index)>Wiki</a>
-            </template>
-        </el-table-column>
-      </el-table>
+        <el-table
+                :data="racersDisplay"
+                style="width: 100%"
+        >
+            <el-table-column
+                    prop="familyName"
+                    label="Family name"
+                    width="180"
+            />
+            <el-table-column
+                    prop="givenName"
+                    label="First name"
+                    width="180"
+            />
+            <el-table-column
+                    prop="dateOfBirth"
+                    label="Date of birth"
+                    width="180"
+            />
+            <el-table-column label="biography">
+                <template slot-scope="scope">
+                    <a v-bind:href= getUrl(scope.$index)>Wiki</a>
+                </template>
+<!--                <template slot-scope="{row}">-->
+<!--                    <a :href=row>Wiki</a>-->
+<!--                </template>-->
+            </el-table-column>
+        </el-table>
         <div class="block">
             <el-pagination
                     layout="prev, pager, next"
                     :total="170"
-                    @current-change="(event) => {changePage(event, id)}"
-            >
-            </el-pagination>
+                    @current-change="changePage"
+            />
         </div>
     </div>
 </template>
@@ -44,27 +46,27 @@
 
     data: () => ({
       racersDisplay: [],
-      id: null,
       racers: []
      }),
 
     mounted(){
-      axios.get(`http://ergast.com/api/f1/drivers.json?limit=847`)
-        .then(response => {
-          this.racers = response.data.MRData.DriverTable.Drivers
           this.changePage(1);
-        })
+        },
 
-
-    },
      methods: {
       getUrl(id){
-         return this.racers[id].url
+         return this.racersDisplay[id].url
       },
-       changePage(id){
-         this.racersDisplay = this.racers.slice(id*50-50, id*50)
-        }
+       changePage(pageNumber){
+         let offset = 50 * pageNumber - 1;
+           this.loadRacers(offset)
+       },
+         loadRacers(offset){
+             axios.get(`http://ergast.com/api/f1/drivers.json?limit=50&offset=${offset}`)
+                 .then(response => {
+                     this.racersDisplay = response.data.MRData.DriverTable.Drivers
+                 })
+         }
      },
-
   }
 </script>
